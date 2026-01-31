53c741150613c4653cfe54aeb6e7e207855dcef3
HaotianLIi <frankyhaotian@gmail.com>
2026-01-31 18:58:48 -0500

    modify default route for root testing

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['status' => 'API is working', 'time' => now()];
 });
 
-Route::get('/students','StudentController@index');
+
+
+
+
