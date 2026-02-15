a7dd9c4488a22e6a95ca541f76e166499e171826
HaotianLIi <frankyhaotian@gmail.com>
2026-02-14 21:54:39 -0500

    modify data naming for the adjustment of column names

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -26,12 +27,12 @@ Route::post('/data/report', function(Request $request) {
             'station_id' => $rawData['PASSKEY'],
             'data_source' => 'ecowitt',
             'temperature_f' => $rawData['tempf'],
-            'humidity' => $rawData['humidity'] ?? null,
-            'wind_direction' => $rawData['winddir'] ?? null,
+            'humidity_pct' => $rawData['humidity'] ?? null,
+            'wind_direction_deg' => $rawData['winddir'] ?? null,
             'wind_speed_mph' => $rawData['windspeedmph'] ?? null,
             'pressure_inhg' => $rawData['pressure_inhg'] ?? null,
-            'rain_rate' => $rawData['rainratein'] ?? null,
-            'solar_radiation' => $rawData['solarradiation'] ?? null,
+            'rain_rate_inph' => $rawData['rainratein'] ?? null,
+            'solar_radiation_wpm2' => $rawData['solarradiation'] ?? null,
             'uv_index' => $rawData['uv'] ?? null,
             'raw_data' => json_encode($rawData),
             'measured_at' => $rawData['dateutc'] ?? now(),
@@ -62,21 +63,22 @@ Route::get('/weatherstation/updateweatherstation.php', function (Request $reques
         if($rawData['ID'] == null) {
             return "\nstation_id is required";
         }
+        $utcTime = $rawData['datautc'] ?? Carbon::now('UTC');
         $data = [
             'station_id' => $rawData['ID'],
             'data_source' => 'wunderstation',
             'temperature_f' => $rawData['tempf'] ?? null,
-            'humidity' => $rawData['humidity'] ?? null,
-            'wind_direction' => $rawData['winddir'] ?? null,
+            'humidity_pct' => $rawData['humidity'] ?? null,
+            'wind_direction_deg' => $rawData['winddir'] ?? null,
             'wind_speed_mph' => $rawData['windspeedmph'] ?? null,
             'pressure_inhg' => $rawData['baromin'] ?? null,
-            'rain_rate' => $rawData['rainin'] ?? null,
-            'solar_radiation' => $rawData['solarradiation'] ?? null,
+            'rain_rate_inph' => $rawData['rainin'] ?? null,
+            'solar_radiation_wpm2' => $rawData['solarradiation'] ?? null,
             'uv_index' => $rawData['UV'] ?? null,
             'raw_data' => json_encode($rawData),
-            'measured_at' => $rawData['dateutc'] ?? now(),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'measured_at' => $utcTime,
+            'created_at' => $utcTime,
+            'updated_at' => $utcTime,
         ];
 
         DB::table('weather_data')->insert($data);
