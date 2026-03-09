c6e5fb4eb5142aadedff6bd486d165bb3a427c3c
Frankie <frankie@AI.ottawa.ad.algonquincollege.com>
2026-03-08 20:02:11 -0400

    update workflows

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+   
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
 }
